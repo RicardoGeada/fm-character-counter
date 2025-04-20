2c524e28e29af298e0b28ad085b2aba3d494e5c9
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-04-20 15:38:21 +0200

    feat: add headline to the layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <Header></Header>
+      <main className="content">
+        <h1 className="headline text-1">Analyze your text in real-time.</h1>
+      </main>
     </>
   );
 }
