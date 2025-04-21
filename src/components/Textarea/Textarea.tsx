6bd11f48ae25443dd4c98e0c302fdf6c93f3a66d
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-04-21 15:44:14 +0200

    feat(textarea): extend UI with textarea-settings

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -4,6 +4,7 @@ import infoIcon from "./../../assets/images/icon-info.svg";
 function Textarea() {
   return (
     <div className={styles["textarea-container"]}>
+
       <textarea
         className={`${styles["textarea"]} text-3`}
         placeholder="Start typing here… (or paste your text)"
@@ -12,6 +13,20 @@ function Textarea() {
         <img src={infoIcon} alt="" />
         <span>Limit reached! Your text exceeds 300 characters.</span>
       </div>
+
+      <div className={`${styles["textarea-settings"]} text-4`}>
+        <label className={styles["checkbox-label"]}>
+            <input type="checkbox"/>
+            <span>Exclude Spaces</span>
+        </label>
+        <label className={styles["checkbox-label"]}>
+            <input type="checkbox" />
+            <span>Set Character Limit</span>
+            <input type="number" min={0} max={999}/>
+        </label>
+        <span>Approx. reading time: 0 minute</span>
+      </div>
+      
     </div>
   );
 }
