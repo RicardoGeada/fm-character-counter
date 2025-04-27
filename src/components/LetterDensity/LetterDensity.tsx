8f2e4dabc604c40fc3d3dc966f4fd96ec11b0f09
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-04-27 11:16:13 +0200

    feat(letter-density): add letter frequency calculation and visualization

--- src/components/LetterDensity/LetterDensity.tsx
+++ src/components/LetterDensity/LetterDensity.tsx
@@ -5,18 +5,48 @@ interface LetterDensityProps {
 }
 
 const LetterDensity: React.FC<LetterDensityProps> = ({text}) => {
+
+    // Alle Buchstaben rausfiltern
+    const uniqueLetters = getUniqueLetters(text);
+    const sortedLetters = [...uniqueLetters].sort((a,b) => {
+      return  getTotal(text, b) - getTotal(text, a);
+    })
+
+    function getUniqueLetters(text: string): string[] {
+      const array = text.replace(/\s+/g, "").toUpperCase().split("");
+      const set = [...new Set(array)];
+      return set;
+    }
+
+    // Buchstaben Prozentsatz errechnen
+    function getTotal(text:string, letter:string) : number {
+      const regex = new RegExp(`[${letter.toUpperCase()}${letter.toLowerCase()}]`, 'g');
+      const total = text.match(regex)?.length || 0;
+      return total;
+    }
+
+    function getPercentage(text:string, letter:string) {
+      const total = getTotal(text, letter);
+      if (total > 0) return ((total / text.replace(/\s+/g, "").length) * 100).toFixed(2);
+      else return 0;
+    }
+    // Der größe nach rendern
+    const letterItemElements = sortedLetters.map((letter) => {
+      return (<li className={styles["letter-item"]} key={letter}>
+        <div className={styles["label"]}>{letter}</div>
+        <div className={styles["progress-bar-container"]}>
+          <div className={styles["progress-bar-fill"]} style={{width:`${getPercentage(text, letter)}%`}}></div>
+        </div>
+        <div className={styles["percent"]}>{getTotal(text, letter)} ({getPercentage(text, letter)}%)</div>
+      </li>)
+    })
+
     return (
         <section className={styles["letter-density-section"]}>
           <h2 className="text-2">Letter density</h2>
-          {text.length == 0 && <p className={`${styles["notification"]} text-4`}>No characters found. Start typing to see letter density.</p>}
+          {text.trim().length === 0 && <p className={`${styles["notification"]} text-4`}>No characters found. Start typing to see letter density.</p>}
           <ul>
-            <li className={styles["letter-item"]}>
-              <div className={styles["label"]}>E</div>
-              <div className={styles["progress-bar-container"]}>
-                <div className={styles["progress-bar-fill"]}></div>
-              </div>
-              <div className={styles["percent"]}>40 (16,06%)</div>
-            </li>
+            {letterItemElements}
           </ul>
         </section>
     );
