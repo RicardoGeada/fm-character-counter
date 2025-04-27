3994396eff8a7747e200c0a2c5fa67bebab9d06a
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-04-28 00:12:46 +0200

    test(letter-density-utils): add tests for getLetterPercentage

--- src/components/LetterDensity/utils/letterDensityUtils.test.ts
+++ src/components/LetterDensity/utils/letterDensityUtils.test.ts
@@ -1,4 +1,4 @@
-import { getLetterCount, getUniqueLetters } from "./letterDensityUtils";
+import { getLetterCount, getLetterPercentage, getUniqueLetters } from "./letterDensityUtils";
 
 describe("getUniqueLetters", () => {
 
@@ -6,26 +6,26 @@ describe("getUniqueLetters", () => {
         const text = "ABCabc";
         const array = getUniqueLetters(text);
         expect(array).toStrictEqual(["A","B","C"])
-    })
+    });
 
     test("should ignore whitespaces when extracting letters", () => {
         const text = "ABC abc";
         const array = getUniqueLetters(text);
         expect(array).toStrictEqual(["A","B","C"])
         expect(array).not.toStrictEqual(["A","B","C", " "])
-    })
+    });
 
     test("should include non-alphabetic characters as unique items", () => {
         const text = "ABC.abc!";
         const array = getUniqueLetters(text);
         expect(array).toStrictEqual(["A","B","C",".","!"])
-    })
+    });
 
     test("should return empty array for empty input", () => {
         const text = "";
         const array = getUniqueLetters(text);
         expect(array).toStrictEqual([]);
-      })
+    });
 
 });
 
@@ -37,27 +37,60 @@ describe("getLetterCount", () => {
         const letter = "A";
         const total = getLetterCount(text, letter);
         expect(total).toStrictEqual(3);
-    })
+    });
 
     test("should be case insensitive when counting characters", () => {
         const text = "ABC. abca";
         const letter = "a";
         const total = getLetterCount(text, letter);
         expect(total).toStrictEqual(3);
-    })
+    });
 
     test("should count non-alphabetic characters correctly", () => {
         const text = "ABC. abca";
         const letter = ".";
         const total = getLetterCount(text, letter);
         expect(total).toStrictEqual(1);
-    })
+    });
 
     test("should return 0 if the character is not present in text", () => {
         const text = "ABC. abca";
         const letter = "Z";
         const total = getLetterCount(text, letter);
         expect(total).toStrictEqual(0);
-    })
+    });
+});
+
+
+describe("getLetterPercentage", () => {
+
+    test("should calculate percentage of a specific character", () => {
+        const text = "ABCD";
+        const letter = "B";
+        const percentage = getLetterPercentage(text, letter);
+        expect(percentage).toStrictEqual(25.00)
+    });
+
+    test("should ignore whitespaces in calculation", () => {
+        const text = "A B C D";
+        const letter = "B";
+        const percentage = getLetterPercentage(text, letter);
+        expect(percentage).toStrictEqual(25.00)
+    });
+
+    test("should handle non-alphabetic characters", () => {
+        const text = "A b . !";
+        const letter = "B";
+        const percentage = getLetterPercentage(text, letter);
+        expect(percentage).toStrictEqual(25.00)
+    });
+
+    test("should be case insensitive", () => {
+        const text = "A b . !";
+        const letter = "b";
+        const percentage = getLetterPercentage(text, letter);
+        expect(percentage).toStrictEqual(25.00)
+    });
+
 })
 
